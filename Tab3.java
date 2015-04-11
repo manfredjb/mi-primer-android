904697896d6a6e6b2491a9dbe440d3d0eaef8539
Manfred <manfred@ensensis.com>
2015-04-10 20:26:33 -0600

    v1

---
+++ Tab3.java
@@ -0,0 +1,20 @@
+package com.manfred.carwash;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by manfred on 4/10/15.
+ */
+public class Tab3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tab3, container, false);
+    }
+}
